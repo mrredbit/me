a484f188df32104d3a8f00344c76ebd8ad1f8705
david <whatdoyou@ttth.ink>
2017-02-12 18:49:55 +0000

    Change assets to relative path

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -68,7 +68,7 @@ function getDefaultModules() {
 
 module.exports = {
     srcPath: srcPath,
-    publicPath: '/assets/',
+    publicPath: 'assets/',
     port: dfltPort,
     getDefaultModules: getDefaultModules
 };
